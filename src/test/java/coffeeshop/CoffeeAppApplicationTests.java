28eaa650cdf629331956717395791a4150c1d6e7
Ugleethyn <tasoskladis@hotmail.gr>
2021-03-05 12:07:31 +0200

    First Push

---
+++ src/test/java/coffeeshop/CoffeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package coffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
