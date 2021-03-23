1fb7ee89f6887c67bf53071f26e97055763a61fa
Ugleethyn <tasoskladis@hotmail.gr>
2021-03-23 14:33:15 +0200

    Cart

--- src/main/java/coffeeshop/controller/UserMenuController.java
+++ src/main/java/coffeeshop/controller/UserMenuController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/user/menu")
--- src/main/java/coffeeshop/service/ProductService.java
+++ src/main/java/coffeeshop/service/ProductService.java
@@ -10,4 +10,6 @@ public interface ProductService {
     public List<Product> findAllDrinks();
 
     public List<Product> findAllSnacks();
+
+    public Product findById(int id);
 }
