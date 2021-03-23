8c06373630033a3cde0026a48ecde9ff03fbd09f
kolobei <72947942+kolobei@users.noreply.github.com>
2021-03-23 10:20:43 +0200

    testing path for css,js

--- src/main/java/coffeeshop/MyWebSecurityConfigurer.java
+++ src/main/java/coffeeshop/MyWebSecurityConfigurer.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,7 +21,7 @@ public class MyWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
-    }
+    }    
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
