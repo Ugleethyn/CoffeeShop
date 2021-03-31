d046260a875b6aad2124c4fe694ea9c7b7e1b9a8
kolobei <72947942+kolobei@users.noreply.github.com>
2021-03-31 18:12:37 +0300

    18:12

--- src/main/java/coffeeshop/service/AccountServiceImpl.java
+++ src/main/java/coffeeshop/service/AccountServiceImpl.java
@@ -5,9 +5,13 @@ import coffeeshop.entity.MyUserDetails;
 import coffeeshop.entity.Role;
 import coffeeshop.repository.AccountRepo;
 import coffeeshop.repository.RoleRepo;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,9 +28,8 @@ public class AccountServiceImpl implements AccountService {
     RoleRepo roleRepo;
     @Autowired
     PasswordEncoder passwordEncoder;
-    
-    
-    public List<Account> getAllAccounts(){
+
+    public List<Account> getAllAccounts() {
         return accountRepo.findAll();
     }
 
@@ -41,20 +44,24 @@ public class AccountServiceImpl implements AccountService {
         Account account = findByUsername(username);
         if (account == null) {
             throw new UsernameNotFoundException("Invalid Username");
+        } else {
+            List<GrantedAuthority> authorities = convertRolesToGrantedAuthorities(account.getRoles());
+            User userOfSpringSecurity = new User(account.getUsername(), account.getPassword(), authorities);
+            return userOfSpringSecurity;
         }
-        return new MyUserDetails(account);
     }
 
-//    private List<GrantedAuthority> convertRolesToGrantedAuthorities(List<Role> roles) {
-//        List<GrantedAuthority> authorities = new ArrayList();
-//        for (Role r : roles) {
-//            GrantedAuthority authority = new SimpleGrantedAuthority(r.getRname());
-//            authorities.add(authority);
-//        }
-//        return authorities;
-//    }
+    private List<GrantedAuthority> convertRolesToGrantedAuthorities(List<Role> roles) {
+        List<GrantedAuthority> authorities = new ArrayList();
+        for (Role r : roles) {
+            GrantedAuthority authority = new SimpleGrantedAuthority(r.getRname());
+            authorities.add(authority);
+        }
+        return authorities;
+    }
+
     @Override
-    public Account saveUser(Account account) {
+    public Account saveUser(Account account){
         String plainPassword = account.getPassword();
         String hashedPassword = passwordEncoder.encode(plainPassword);
         account.setPassword(hashedPassword);
@@ -64,20 +71,12 @@ public class AccountServiceImpl implements AccountService {
         return account;
     }
 
-    public Account getCurrentlyLoggedInAccount(Authentication authentication) {
-        if (authentication == null) {
-            return null;
-        }
-        Account account = null;
-        account = ((MyUserDetails) authentication.getPrincipal()).getAccount();
-        return account;
-    }
-
+    
 
     public List<Account> getAdmins() {
         return accountRepo.findByRoleId(1);
     }
-    
+
     public List<Account> getUsers() {
         return accountRepo.findByRoleId(2);
     }
